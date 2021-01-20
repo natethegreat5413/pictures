169efbd3f4172d66da93b1b0c93b6f5ada5024a6
Nathan Cowley <nathancowley12@gmail.com>
2021-01-20 16:04:52 -0700

    now retrieving information from the unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+// components //
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios
+			.get('https://api.unsplash.com/search/photos', {
+				params: { query: term },
+				headers: {
+					Authorization: 'Client-ID Ir667BlJRr6hV1kVrSjMYmtN24_JCONFXbgm3kNIrQY'
+				}
+			})
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	}
 	render() {
 		return (
